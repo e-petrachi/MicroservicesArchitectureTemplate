d1f4437266c2a14a55e1956e6895d0b097f671b3
MarcoZebi <marcoze19@gmail.com>
2017-06-20 11:36:35 +0200

    Update start-brands-stack-swarm.sh

--- progetti/brands_info_swarm/scripts/start-brands-stack-swarm.sh
+++ progetti/brands_info_swarm/scripts/start-brands-stack-swarm.sh
@@ -2,7 +2,7 @@
 
 echo 'Starting brands-info application as a stack'
 
-#source "docker.env"
+
 
 export DOCKER_HOST=tcp://swarm.inf.uniroma3.it:2376
 export DOCKER_CERT_PATH=../swarm.inf.uniroma3.it/resources/certs
